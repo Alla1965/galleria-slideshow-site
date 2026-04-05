bdd235a675a384177dc06754220e59852606310c
Alla1965 <savanna.opt@gmail.com>
2026-04-05 21:01:09 +0300

    4

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
+import { resolve } from 'path'
 
 export default defineConfig({
+  base: './', // 🔥 важно для Vercel
+
   plugins: [tailwindcss()],
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        detail: resolve(__dirname, 'detail.html'),
+      },
+    },
+  },
 })
